28a0a9ed463452b12c3904d0b299ccc621c05cbd
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-03-24 10:05:01 +0100

    Update gameUnique.js

--- js/gameUnique.js
+++ js/gameUnique.js
@@ -7,7 +7,6 @@ const url = 'https://api.rawg.io/api/games/' + id + '?key=fc28d25bb9a8458487040d
 const gamesMonth = 'https://api.rawg.io/api/games?ordering=-metacritic&dates=2022-02-01,2022-03-22' + '/' + id + '&key=fc28d25bb9a8458487040dc95d300dff';
 
 async function apiGame() {
-    try {
         const fetchApi = await fetch(url);
         const callApi = await fetchApi.json();
         const games = callApi.results;
@@ -19,9 +18,5 @@ async function apiGame() {
         <h2>${games.name}</h2>
         </div>
         `
-    } catch(error) {
-        console.log(error);
-        html.innerHTML = displayError(), "ERROR HAS OCCURED!";
-    }
 }
 apiGame();
