46b567883f4eaf5333d0f8e067e9b98b1b35b32f
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-03-24 09:43:41 +0100

    Update gameUnique.js

--- js/gameUnique.js
+++ js/gameUnique.js
@@ -3,27 +3,24 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const html = document.querySelector(".apiContainer");
-const url = 'https://api.rawg.io/api/games?ordering=-metacritic&key=fc28d25bb9a8458487040dc95d300dff' + id;
-const gamesMonth = "https://api.rawg.io/api/games?ordering=-metacritic&dates=2022-02-01,2022-03-22&key=fc28d25bb9a8458487040dc95d300dff" + id;
+const url = 'https://api.rawg.io/api/games?ordering=-metacritic&key=fc28d25bb9a8458487040dc95d300dff' + '/' + id;
+const gamesMonth = "https://api.rawg.io/api/games?ordering=-metacritic&dates=2022-02-01,2022-03-22&key=fc28d25bb9a8458487040dc95d300dff" + '/' + id;
 
 async function apiGame() {
     try {
         const fetchApi = await fetch(url);
         const callApi = await fetchApi.json();
         const games = callApi.results;
+        document.title = `${games.name}`;
         html.innerHTML = '';
-        for(let i = 0; i < games.length; i++) {
-            document.title = `${games[i].name}`;
-            console.log(games[i]);
-            html.innerHTML = `
-            <div class="uniqueGame">
-            <h2>${games[i].name}</h2>
-            </div>
-            `
-        }
+        console.log(games);
+        html.innerHTML += `
+        <div class="uniqueGame">
+        <h2>${games.name}</h2>
+        </div>
+        `
     } catch(error) {
         console.log(error);
         html.innerHTML = displayError(), "ERROR HAS OCCURED!";
     }
 }
-apiGame();
