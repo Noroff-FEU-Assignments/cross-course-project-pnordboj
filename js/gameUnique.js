4eaac90eb2ba9992a0344c375a904f944330cba5
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-03-25 11:40:38 +0100

    Update gameUnique.js

--- js/gameUnique.js
+++ js/gameUnique.js
@@ -3,19 +3,36 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const html = document.querySelector(".apiContainer");
+const addCart = document.querySelector(".addCart");
 const url = 'https://api.rawg.io/api/games/' + id + '?key=fc28d25bb9a8458487040dc95d300dff';
 
 async function apiGame() {
-        const fetchApi = await fetch(url);
-        const games = await fetchApi.json();
-        console.log(games);
-        html.innerHTML = '';
-        document.title = `${games.name}`;
-        console.log(games);
-        html.innerHTML += `
-        <div class="uniqueGame">
-        <h2>${games.name}</h2>
-        </div>
-        `;
+        try {
+                const fetchApi = await fetch(url);
+                const games = await fetchApi.json();
+                console.log(games);
+                html.innerHTML = '';
+                document.title = `${games.name}`;
+                console.log(games);
+                html.innerHTML += `
+                <div class="uniqueGame">
+                <h2>${games.name}</h2>
+                <img src="${games.background_image}" id="apiImage" alt="${games.name}(Image Missing)">
+                <p id="apiDescription">${games.description}</p>
+                <p id="apiRelease">${games.released}</p>
+                </div>`;
+                addCart.innerHTML += `
+                <div class="addContainer">
+                <a id="buttonRef" href="checkout.html?id=${games.id}">
+                        <button id="complete">
+                                Purchase ${games.name}
+                        </button>
+                </a>
+                </div>
+                `;
+        } catch(error) {
+                console.log(error);
+                html.innerHTML = displayError(error, 'An error has occured!');
+        }
 }
 apiGame();
